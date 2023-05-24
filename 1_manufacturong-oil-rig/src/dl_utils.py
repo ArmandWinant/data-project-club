075f2451520a858e46b1339ffe2c3049906de878
Bastien Winant <babw1@student.london.ac.uk>
2023-05-24 12:28:36 +0200

    extract file modify time

--- 1_manufacturong-oil-rig/src/dl_utils.py
+++ 1_manufacturong-oil-rig/src/dl_utils.py
@@ -1,12 +1,17 @@
 import os
 import sqlite3
-from sql_queries import dl_drop_table_queries, dl_create_table_queries
+from src.sql_queries import dl_drop_table_queries, dl_create_table_queries
 
 
-def dl_connect():
+def dl_connect(target_directory):
     '''
     Open a connection to the sensor_data database and return con and cur objects.
     '''
+    try:
+        os.chdir(target_directory)
+    except FileNotFoundError:
+        os.mkdir(target_directory)
+        os.chdir(target_directory)
     con = sqlite3.connect("sensor_data.db")
     cur = con.cursor()
     
@@ -24,10 +29,17 @@ def dl_close(con, cur):
         print("Database connection is already closed")
 
 
-def dl_create_tables():
-    con, cur = dl_connect()
+def dl_create_tables(target_directory):
+    '''
+    Create database if not exists in the target directory
+    '''
+    current_directory = os.getcwd()
+    
+    con, cur = dl_connect(target_directory)
     
     for query in dl_create_table_queries:
         cur.execute(query)
         
     dl_close(con, cur)
+    
+    os.chdir(current_directory)
