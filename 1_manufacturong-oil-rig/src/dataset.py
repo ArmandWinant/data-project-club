3e8562c4214d358197941511e7b82c7a4851afcb
Bastien Winant <babw1@student.london.ac.uk>
2023-06-03 16:50:20 +0200

    add safe exit for file not found

--- 1_manufacturong-oil-rig/src/dataset.py
+++ 1_manufacturong-oil-rig/src/dataset.py
@@ -13,7 +13,13 @@ class DataSet:
         self.__data = None
         
     def read_data(self):
-        self.__data = pd.read_csv(os.path.join(self.__data_dir, self.__file_name), delimiter="\t", header=None)
+        try:
+            self.__data = pd.read_csv(os.path.join(self.__data_dir, self.__file_name), delimiter="\t", header=None)
+
+            self.__cycleCount = self.__data.shape[0]
+            self.__sampleCount = self.__data.shape[1]
+        except FileNotFoundError:
+            print(f"Could not file '{self.__file_name}' in folder '{self.__data_dir}'")
     
     def cycle_metadata(self):
         try:
@@ -25,32 +31,42 @@ class DataSet:
         sensor_type = re.search(r"^[a-zA-Z]+", self.__file_name).group()
         sensor_number = re.search(r"(\d)+", self.__file_name).group()
         
-        return sensor_type, sensor_number
+        self.sensor_type = sensor_type
+        self.sensor_number = sensor_number
     
-    def sensor_meta_data(self):
-        sensor_type, sensor_number = self.get_sensor_name()
+    def sensor_metadata(self):
         try:
-            self.__data["sensorType"] = sensor_type
-            self.__data["sensorNumber"] = sensor_number
-        except AttributeError:
+            self.__data["sensorType"] = self.sensor_type
+            self.__data["sensorNumber"] = self.sensor_number
+        except TypeError:
             print("The data has not been loaded.")
         
     def cycle_to_sample(self):
         try:
-            long_data = self.__data.melt(id_vars=["cycleNumber"], var_name="metric", value_name="value")
-            long_data["sampleNumber"] = long_data.index + 1
-            
-            print(long_data.head())
+            long_data = self.__data.melt(id_vars=["cycleNumber", "sensorType", "sensorNumber"], var_name="sampleNumber", value_name="value")
+            long_data["sampleNumber"] += 1
+            long_data.sort_values(by=["cycleNumber", "sampleNumber"], inplace=True)
         except AttributeError:
             print("The data has not been loaded.")
         except KeyError:
             print("Cound not find cycle number.")
+    
+    def get_data(self):
+        return self.__data
 
             
 class SensorDataSet(DataSet):
     def __init__(self, filename):
         super().__init__(filename)
-        self.sensor_type, self.sensor_number = self.get_sensor_name()
+        
+        self.get_sensor_name()
+        
+    def extractTransformLoad(self):
+        self.read_data()
+        self.cycle_metadata()
+        self.sensor_metadata()
+        self.cycle_to_sample()
+        
 #     def metrics_upload():
 #         pass
     
