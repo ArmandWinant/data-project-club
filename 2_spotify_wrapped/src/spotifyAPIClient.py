aa42cd3df4d20bb48bf2c85e15320aae8894c52f
Bastien Winant <babw1@student.london.ac.uk>
2023-06-25 16:02:57 +0200

    create wrapper for safe api calls

--- 2_spotify_wrapped/src/spotifyAPIClient.py
+++ 2_spotify_wrapped/src/spotifyAPIClient.py
@@ -28,8 +28,18 @@ class SpotifyAPIClient:
         
         if r.status_code == 200:
             self.access_token = r.json()
-        
-    def get_artist_info(self, artist_id):
-        url = "https://api.spotify.com/v1/artists/6rqlONGmPuP2wJVSfliLBI"
-        headers = {"Authorization": f"{token_type} {access_token}"}
-        artist_data = requests.get(url, headers=headers)
+    
+    def api_request(self, url, *args, **kwargs):
+        # TODO: verify that the access token is defined
+        try:
+            headers = {"Authorization": f"{self.access_token['token_type']} {self.access_token['access_token']}"}
+            artist_data = request.get(url, headers=headers)
+        except AttributeError:
+            print("SpotifyAPIClient.api_request(): Access Token was not generated (call SpotifyAPIClient.get_access_token())")
+            return
+        return artist_data
+    
+#     def get_artist_info(self, artist_id):
+#         url = "https://api.spotify.com/v1/artists/6rqlONGmPuP2wJVSfliLBI"
+#         headers = {"Authorization": f"{sc.access_token['token_type']} {sc.access_token['access_token']}"}
+#         artist_data = requests.get(url, headers=headers)
